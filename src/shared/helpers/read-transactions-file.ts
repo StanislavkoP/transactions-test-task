b0cbfa86fa6ff18c87b47b6fa5ba1e370d0b3eb7
stanislav.pastushenko <stas8327634@gmail.com>
2023-04-06 19:17:56 +0300

    refactor: rename file

---
+++ src/shared/helpers/read-transactions-file.ts
@@ -0,0 +1,27 @@
+import fsSync from 'fs'
+import fs from 'fs/promises'
+import path from 'node:path'
+import { ITransaction } from '../types/transaction'
+
+export async function readTransactionsFile(name: string): Promise<ITransaction[]> {
+  const filePath = path.join(__dirname, `../assets/${name}`)
+  const isFileExists = fsSync.existsSync(filePath)
+
+  if (!isFileExists) {
+    throw new Error(`File not exists by ${filePath}`)
+  }
+
+  const data = await fs.readFile(filePath, 'utf-8')
+
+  if (!data) {
+    throw new Error(`File with a name ${name} is empty`)
+  }
+
+  const transactions: { data: ITransaction[] } = JSON.parse(data)
+
+  if (!transactions.data?.length) {
+    throw new Error(`Transaction list is empty`)
+  }
+
+  return transactions.data
+}
