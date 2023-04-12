5cc3644a3b278adb88299d3994182f4335b00a56
stanislav.pastushenko <stas8327634@gmail.com>
2023-04-12 09:53:45 +0300

    feat: add transformIgnorePatterns

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ export default {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -31,7 +31,7 @@ export default {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -176,11 +176,7 @@ export default {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
-
+  transformIgnorePatterns: ['node_modules/(?!variables/.*)', 'build/(?!variables/.*)'],
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
 
@@ -192,4 +188,4 @@ export default {
 
   // Whether to use watchman for file crawling
   // watchman: true,
-};
+}
